c78169b5efb4693bf1d36601c29e424df28e6e09
Eyad Osama <eyadosama0616@gmail.com>
2026-06-04 06:52:20 +0300

    feat(webhook): initializing the enviroment

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Jobs\ProcessWebhookJob;
 
 class WebhookController extends Controller
 {
-    public function handle(Request $request)
+    public function handle(Request $request, $bank)
     {
+        //getContent to get the raw data from the request
+        $data = $request->getContent();
+
+        ProcessWebhookJob::dispatch($data, $bank);
+
+        return response()->json([
+            'message' => 'Webhook received',
+        ], 202);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebhookController;
 
 Route::get('/test', function () {
     return response()->json([
         'message' => 'API is working'
     ]);
 });
+
+Route::post('/webhooks/{bank}', [WebhookController::class, 'handle']);
